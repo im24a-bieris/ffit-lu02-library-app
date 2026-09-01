f1599daaa3a6e985a50f1270cf46b288d3c8ad61
im24a-bieris <bieris@bzz.ch>
2026-09-01 10:47:28 +0200

    Book-Klasse und listBooks implementiert

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class LibraryAppMain {
 
+    private static final Book BOOK_1 = new Book(1, "978-3-8362-9544-4", "Java ist auch eine Insel", "Christian Ullenboom", 2023);
+    private static final Book BOOK_2 = new Book(2, "978-3-658-43573-8", "Grundkurs Java", "Dietmar Abts", 2024);
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,7 +16,10 @@ public class LibraryAppMain {
             if (eingabe.equals("quit")) {
                 break;
             } else if (eingabe.equals("help")) {
-                System.out.println("Verfügbare Befehle: help, quit");
+                System.out.println("Verfügbare Befehle: help, quit, listBooks");
+            } else if (eingabe.equals("listBooks")) {
+                System.out.println(BOOK_1.getTitle());
+                System.out.println(BOOK_2.getTitle());
             } else {
                 System.out.println("Befehl nicht erkannt: " + eingabe);
             }
