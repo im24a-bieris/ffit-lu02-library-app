41bb84beae8a7fe6def46de55066ed51e9b2e84f
im24a-bieris <bieris@bzz.ch>
2026-09-01 09:17:28 +0200

    efixxt

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -6,10 +6,17 @@ public class LibraryAppMain {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        String eingabe = scanner.nextLine();
-        
-        System.out.println("Du hast eingegeben: " + eingabe);
+
+        while (true) {
+            String eingabe = scanner.nextLine();
+
+            if (eingabe.equals("quit")) {
+                break;
+            } else if (eingabe.equals("help")) {
+                System.out.println("Verfügbare Befehle: help, quit");
+            } else {
+                System.out.println("Befehl nicht erkannt: " + eingabe);
+            }
+        }
     }
-    
 }
